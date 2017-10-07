e8eb0e38efe53314ce21b096cef19b8e85aa961d
Jason McNew <foonix@yahoo.com>
2017-10-07 16:28:01 -0700

    Move enums to associated class files.

--- XwOpt/OptNode/PartDescriptor.cs
+++ XwOpt/OptNode/PartDescriptor.cs
@@ -5,6 +5,42 @@ using System.IO;
 
 namespace SchmooTech.XWOpt.OptNode
 {
+    public enum PartType
+    {
+        DefaultType,
+        MainHull,
+        Wing,
+        Fuselage,
+        GunTurret,
+        SmallGun,
+        Engine,
+        Bridge,
+        ShieldGen,
+        EnergyGen,
+        Launcher,
+        CommSys,
+        BeamSys,
+        CommandBeam,
+        DockingPlat,
+        LandingPlat,
+        Hangar,
+        CargoPod,
+        MiscHull,
+        Antenna,
+        RotWing,
+        RotGunTurret,
+        RotLauncher,
+        RotCommSys,
+        RotBeamSys,
+        RotCommandBeam,
+        Custom1,
+        Custom2,
+        Custom3,
+        Custom4,
+        Custom5,
+        Custom6,
+    }
+
     public class PartDescriptor : BaseNode
     {
         public PartDescriptor(OptFile opt) : base(opt)
