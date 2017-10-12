90e627b32a33cad5fedeb74e2d351ec4411bd180
Jason McNew <foonix@yahoo.com>
2017-10-12 05:12:39 -0700

    Switch to .net framework 3.5

--- XWOpt_test/XwOpt_test.cs
+++ XWOpt_test/XwOpt_test.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SchmooTech.XWOpt;
+﻿using SchmooTech.XWOpt;
 using NUnit.Framework;
 
 namespace XWOpt_test
